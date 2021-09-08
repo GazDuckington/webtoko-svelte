f5d5376abce27d17d31552ba331d5e33900fa4a2
Gaz <dianghazy@protonmail.com>
2021-09-09 02:50:54 +0700

    master

--- src/lib/comps/navbar.svelte
+++ src/lib/comps/navbar.svelte
@@ -26,23 +26,23 @@ import Linky from "$lib/atoms/linky.svelte";
 
 <!-- quick cart -->
 {#if qcart}
-        <div class="quick-cart rounded fixed">
-            <div class="flex justify-between p-1  bg-gradient-to-r from-green-500">
-                <h1 class="text-white">Cart</h1>
-                <button class="text-red-500" on:click={qCart}>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
-                </svg>
-                </button>
-            </div>
-            <div class="cart-view p-2">
-                <ul>
-                    <li>something for nothing</li>
-                    <li>something for nothing</li>
-                    <li>something for nothing</li>
-                </ul>
-            </div>
+    <div class="quick-cart rounded fixed">
+        <div class="flex justify-between p-1  bg-gradient-to-r from-green-500">
+            <h1 class="text-white">Cart</h1>
+            <button class="text-red-500" on:click={qCart}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clip-rule="evenodd" />
+            </svg>
+            </button>
         </div>
+        <div class="cart-view p-2">
+            <ul>
+                <li>something for nothing</li>
+                <li>something for nothing</li>
+                <li>something for nothing</li>
+            </ul>
+        </div>
+    </div>
 {/if}
 <div class="navbar">
     <div class="navlogo">
