5cadb40ce6bfa60fa2a2cd97d3e6917542750719
--global <--global>
2021-09-15 12:11:04 +0700

    added: info

---
+++ info.md
@@ -0,0 +1,15 @@
+# KAPE
+koperasi pekuncen. kemeranjen. <br>
+## Produksi:
+- gula dari lokal
+- B2B
+## fitur:
+- i/o barang
+- jumlah anggota
+- daftar anggota
+## PRIORITAS:
+Halaman Portofolio
+<br>
+pak awiet: 082135331617
+bu gustin: 085747189391 
+pak toni: 085747999476
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,10 @@
 <script>
+import Button from "$lib/atoms/button.svelte";
 import MenuUser from "$lib/atoms/menuUser.svelte";
 import Title from "$lib/atoms/title.svelte";
+import {products} from "$lib/db";
+
+// products.subscribe((value) => {value});
 </script>
 
 <svelte:head>
@@ -12,3 +16,7 @@ import Title from "$lib/atoms/title.svelte";
 </Title>
 
 <MenuUser/>
+ <Button>Test</Button>
+<!-- {#each $products as p}
+{p}
+{/each} -->
